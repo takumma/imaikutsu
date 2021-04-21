c70af65afc7a371c56f9e450e99be47036849fb6
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-04-22 00:14:47 +0900

    Add: ChakraUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
