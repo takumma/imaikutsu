b73658ed8f0ae9ff67e0273097f1cb574c6bd49b
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-04-24 00:21:11 +0900

    Update: add firebase in next

---
+++ firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyA1JO6wDzzpYQefMB6vJRrfg58_gjALY3U",
+  authDomain: "imaikutsu.firebaseapp.com",
+  projectId: "imaikutsu",
+  storageBucket: "imaikutsu.appspot.com",
+  messagingSenderId: "607654962168",
+  appId: "1:607654962168:web:06efe8f53c27bec80c2c0d"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+module.exports = {
+  
+}
