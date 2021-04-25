1d205acada261f680a216292d5f93fff9adea65b
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-04-26 01:06:59 +0900

    getMentalValueFromName

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,27 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+export const getMentalValue = functions.https.onRequest((request, response) => {
+  
+  const num = getMentalValueFromName((request.query as any).name!)
+  response.send({
+    mentalValue: num
+  });
+});
+
+const getMentalValueFromName = (name: string): number | null => {
+  const doubleDigit = Number(name.slice(-2))
+  const singleDigit = Number(name.slice(-1))
+
+  if(isNaN(doubleDigit)) {
+    if(!isNaN(singleDigit)) return singleDigit
+    else return null
+  } else {
+    if(doubleDigit > 10) return singleDigit
+    else return doubleDigit
+  }
+}
