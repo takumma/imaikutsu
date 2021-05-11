f771a110fe28aee245238c4bbcf7d42a228677ce
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-05-12 00:56:45 +0900

    WiP: add swr

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -2,9 +2,9 @@ import { Box, Button } from "@chakra-ui/react"
 import styles from '../styles/Home.module.css'
 import Graph from '../components/Graph'
 import { useRouter } from 'next/router';
-import { useEffect, useState } from "react";
 
 import firebase from '../utils/firebase'
+import useSWR from "swr";
 
 const firestore = firebase.firestore()
 
@@ -12,32 +12,23 @@ const firestore = firebase.firestore()
 const UserPage = () => {
   const router = useRouter();
   const { user } = router.query;
-  const [data, setData] = useState([])
-
-  // useEffect(() => {
-  //   const getGraphData = async () => {
-  //     await firestore.collection('graphs').where("screen_name", "==", user).limit(1).get().then((snapshot) => {
-  //       snapshot.forEach((doc) => {
-  //         const mentalValues = doc.data().mentalValues
-  //         if (mentalValues) setData(mentalValues)
-  //       })
-  //     })
-  //     .catch((err) => console.error(err));
-  //   }
-
-  //   getGraphData()
-  
-  // }, [])
 
   const getGraph = async () => {
-    await firestore.collection('graphs').where("screen_name", "==", user).limit(1).get().then((snapshot) => {
+    return await firestore.collection('graphs').where("screen_name", "==", user).limit(1).get().then((snapshot) => {
       snapshot.forEach((doc) => {
+        console.log('res')
         const mentalValues = doc.data().mentalValues
-        if (mentalValues) setData(mentalValues)
+        if (!mentalValues) return []
+        return mentalValues        
       })
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      console.error(err)
+      return []
+    });
   }
+
+  const { data, error } = useSWR('getgraph', getGraph)
 
   return (
     <Box bg='#FFDE59'>
