971ecbe38755cc0b0c16070fa8121951071bdd83
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2021-05-12 23:30:44 +0900

    Fix

--- pages/[user].tsx
+++ pages/[user].tsx
@@ -3,8 +3,8 @@ import styles from '../styles/Home.module.css'
 import Graph from '../components/Graph'
 import { useRouter } from 'next/router';
 
-import firebase from '../utils/firebase'
 import useSWR from "swr";
+import getGraph from "../utils/getGraph";
 
 
 const UserPage = () => {
@@ -25,7 +25,6 @@ const UserPage = () => {
     <Box bg='#FFDE59'>
       <div className={styles.container}>
         { user }
-        <Button onClick={() => getGraph(`${user}`)}>get</Button>
         <Graph data={data}/>
       </div>
     </Box>
@@ -33,23 +32,3 @@ const UserPage = () => {
 }
 
 export default UserPage
-
-const getGraph = async (user: string) => {
-  const collection = firebase.firestore().collection('graphs')
-
-  console.log(collection)
-  console.log(user)
-
-  return await collection.where("screen_name", "==", `${user}`).limit(1).get().then((snapshot) => {
-    let result = []
-    snapshot.forEach((doc) => {
-      const mentalValues = doc.data().mentalValues
-      if(mentalValues) result = mentalValues
-    })
-    return result
-  })
-  .catch((err) => {
-    console.error(err)
-    return []
-  });
-}
