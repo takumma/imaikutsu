bf1e663d2a1f8c18a5dae73c74e5014d294c836d
takumma <h30i07@ed.cc.suzuka-ct.ac.jp>
2022-01-09 15:30:45 +0900

    Fix

--- functions/src/declarations.d.ts
+++ functions/src/declarations.d.ts
@@ -11,10 +11,6 @@ declare namespace config {
    */
   interface Config {
     functions: {
-      consumer_key: string;
-      consumer_secret: string;
-      access_token_key: string;
-      access_token_secret: string;
       bearer: string;
     };
   }
